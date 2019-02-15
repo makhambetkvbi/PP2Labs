7594c75389a7a75766db6dad950102ac1b6b2fd2
makhambetkabi <kabi-2001@mail.ru>
2019-02-15 16:21:49 +0600

    lab1

---
+++ week1/Task2/Task2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Program
+{
+    class Student // создаю класс 
+    {
+        public string name;
+        public string id;  // идентифицироваю
+
+        public Student(string name, string id)
+        {
+            this.name = name;
+            this.id = id;  // обеспечиваю доступ к текущему экземпляру класса с помощью ключевого слова this
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student st1 = new Student(Console.ReadLine(), Console.ReadLine()); // считываю с консоли name, потом id и передаю в класс
+
+            int year = int.Parse(Console.ReadLine());
+
+            Console.Write(st1.name + " " + st1.id + " " + ++year); // вывожу с инкрементом year
+            Console.ReadKey();
+        }
+    }
+}
---
+++ week1/Task3/Task3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];                   // создал массив
+
+
+            string[] str = Console.ReadLine().Split();
+
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(str[i]);  // заполнил массив 
+            }
+
+            Dup(arr, n); // передал методу Dup
+
+        }
+
+        public static void Dup(int[] arr2, int m)
+        {
+            List<int> repeat = new List<int>(); // создал список
+
+            for (int i = 0; i < m; i++)
+            {
+                repeat.Add(arr2[i]);
+                repeat.Add(arr2[i]); // заполнил два раза
+            }
+
+            for (int i = 0; i < repeat.Count; i++)
+            {
+                Console.Write(repeat[i] + " "); // вывод 
+            }
+            Console.ReadKey();
+        }
+    }
+}
