1d2a64a9ab5f7e42797d6ad9c8853a2ce27a16d7
makhambetkabi <kabi-2001@mail.ru>
2019-02-15 17:02:21 +0600

    lab2

---
+++ week2/task1/task1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader("../../input.txt");
+            string n = sr.ReadToEnd();
+            int b = n.Length;
+            for(int i = 0; i < b; i++)
+            {
+                if (n[i] != n[b - i - 1])
+                {
+                    Console.WriteLine("No");
+                    Console.ReadKey();
+                    Environment.Exit(0);
+                }
+            }
+            Console.WriteLine("Yes");
+            Console.ReadKey();
+            sr.Close();
+        }
+    }
+}
