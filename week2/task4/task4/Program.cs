9cc562551cb11179d49d72e2ac1964eab4fb0cf4
makhambetkabi <kabi-2001@mail.ru>
2019-02-16 00:29:44 +0600

    lab2

---
+++ week2/task4/task4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task4
+{
+    class Program
+    {
+        public static void Start()
+        {
+            string origin = @"C:\Users\Махамбет\Desktop\PP2Labs\week2\task4\path";
+            string fileName = Console.ReadLine();
+
+            origin = Path.Combine(origin, fileName);
+
+            File.WriteAllText(origin, "len pisat comment");
+
+            string copy = @"C:\Users\Махамбет\Desktop\PP2Labs\week2\task4\path1";
+            string copyfile = Path.Combine(copy, fileName);
+
+            File.Copy(origin, copyfile, true);
+            Delete(origin);
+        }
+
+        public static void Delete(string path)
+        {
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+            }
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            Start();
+        }
+    }
+}
---
+++ week2/task4/task4/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("task4")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("task4")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("ffe5a601-e1bb-4259-a09b-d8c7a4890228")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
